b33c854c3213b35b11ab7bc6582efba9ad5328f9
Marcelo Júnior <rokermarcelo@gmail.com>
2018-08-15 10:09:10 -0300

    Definição inicial dos metadados da página inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Marcelo Júnior">
+        <meta name="keywords" content="Portfólio pessoal, Marcelo Júnior, trabalhos, Web">
+        <meta name="description" content="Portifólio pessoal do prof. Marcelo Júnior, que 
+              atua nas áreas de desenvolvimento Web com PHP/HTML/CSS/Javascript e apps Android">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Marcelo Júnior</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
