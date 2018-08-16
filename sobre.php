3552e9db63a97e60f0b9d57f3af3bd64533e1985
Marcelo Junior <rokermarcelo@gmail.com>
2018-08-16 11:01:15 -0300

    Adicionada página sobre o autor

---
+++ sobre.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Marcelo Júnior">
+        <meta name="keywords" content="Portfólio pessoal, Marcelo Júnior, trabalhos, Web">
+        <meta name="description" content="Portifólio pessoal do prof. Marcelo Júnior, que 
+              atua nas áreas de desenvolvimento Web com PHP/HTML/CSS/Javascript e apps Android">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Marcelo Júnior</title>
+
+        <link rel="stylesheet" href="css/geral.css">
+        <link rel="icon" href="img/h1.png">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <span></span>
+                Marcelo Júnior
+            </h1>
+        </header>
+        <div id="imagemGrande"></div>
+        <section id="geral">
+            <h2>Sobre Marcelo Júnior</h2>
+            <figure class="autor">
+                    <img src="img/trabalho.png" alt="Trabalho 001">
+                    <figcaption>Marcelo Júnior</figcaption>
+            </figure>
+            <article>
+                <h3>Pessoal</h3>
+                <p>
+                    Sou casado desde 2016 e amo minha esposa, ainda não temos filhos,
+                    mas aguardo ancioso pela chegada deles!
+                </p>
+                <p>
+                    Gosto de viajar, atualmente estou tentando manter uma média
+                    de uma viagem a cada um ou dois meses, estou contando com a AINA
+                    para isso.
+                </p>
+                <p>
+                    Também gosto de pilotar moto, principalmente pegar a estrada para
+                    rumos distantes (viajar). E atualmente com uma CB 500F da Honda
+                    minhas viagens estão muito mais agradáveis e seguras.
+                </p>
+                <p>
+                    Tenho uma certa habilidade para desenhar, gosto de desenhar
+                    o rosto das pessoas (bonitas), mas esse hobby não tem sido
+                    muito estimulado... uma pena!
+                </p>
+            </article>
+            <article>
+                <h3>Profissional</h3>
+                <p>
+                    Atualmente trabalho como professor de informática/programação
+                    do ensino médio/técnico no IFRN campus Santa Cruz. Adoro dar aulas,
+                    principalmente quando os alunos demonstram empolgação com o
+                    conteúdo e que estão aprendendo. Por isso até hoje nunca repeti
+                    uma disciplina, sempre reformulando a forma de passar o conteúdo
+                    tentando atingir os alunos de uma forma eficiente.
+                </p>
+                <p>
+                    Claro que ainda tenho que aprender sobre tecnicas de ensino,
+                    mas acredito que minha inquietação está no caminho certo!
+                </p>
+            </article>
+        </section>
+    </body>
+</html>
